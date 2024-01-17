f05fed62177478110b63a66ae8999d8a14e6f0e7
raja-8820 <esakkipandi12015@gmail.com>
2024-01-17 17:07:11 +0530

    modified

--- file3.py
+++ file3.py
@@ -1,1 +1,7 @@
-print("hello world")
+step 1 : create file
+step 2 : git  add file3.py   #goes to staging area 
+step 3 : git commit --m "Any commited commend"   
+step 4 : git push origin main #it is used to push file from system to git page
+                              #origin refers to reposrity link , main refers to main branch
+step 5 : git status  # it is used to check the file status in git 
+ 
